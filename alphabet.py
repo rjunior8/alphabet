878717cea6738738a97ea1a20d51eb6af9726559
rjunior8 <r_junior@protonmail.com>
2018-10-03 13:08:50 -0300

    Octuber - 3rd - 2018

--- alphabet.py
+++ alphabet.py
@@ -9,24 +9,24 @@ alphabet = string.ascii_lowercase
 counter = 0
 
 for i in alphabet:
-    counter += 1
-    d1.update({str(counter) : i})
+  counter += 1
+  d1.update({str(counter) : i})
 
 sequence = input("Enter with the sequence: ")
 sequence = sequence.split(" ")
 
 for n in sequence:
-    if n in d1:
-        l1.append(d1[n])
-        l2.append(d1[n])
+  if n in d1:
+    l1.append(d1[n])
+    l2.append(d1[n])
 
 print("".join(l1))
 
 while True:
-    choice = input("Enter the letter(s) here: ")
-    l3.append(choice)
+  choice = input("Enter the letter(s) here: ")
+  l3.append(choice)
 
-    for j in choice:
-        l2.remove(j)
+  for j in choice:
+    l2.remove(j)
 
-    print(l3, "\t" + "".join(l2))
+  print(l3, "\t" + "".join(l2))
